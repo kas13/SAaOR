5048f5cff64bef6affed4b4cc8a323279fe8033e
kas13 <itklon@gmail.com>
2018-12-12 16:17:20 +0300

    8lab: add more functions

--- 8lab.py
+++ 8lab.py
@@ -1,5 +1,6 @@
 #G = [[[1,5], [2,3]],[[2,3],[3,3]],[[4, 5]],[[2,4],[4,7]],[[]]]
-G = [[[1,50]],[[3,30],[2,20]],[[5, 19]],[[4,31]],[[0,10]]]
+#G = [[[1,50], [2,10]],[[3,30],[2,20]],[[5, 70]],[[4,31]],[[5,10]]]
+G = [[[1,44], [3,11]],[[2,89],[3,23]],[[4, 68]],[[2,24],[4,36]],[[]]]
 D = []
 U = []
 
@@ -17,10 +18,9 @@ def dfs(t):
 		for edge in node:
 			print("  edge   ", edge)
 			if edge[0] == t:
-				print("victori")
+				#print("victori")
 				path.append(edge[0])
-				print(path, min_flow)
-				return path, min_flow
+				return path
 			if U[edge[0]] is True:
 				continue
 			elif edge[1] > edge[2]:
@@ -28,47 +28,84 @@ def dfs(t):
 				stack.append(G[edge[0]])
 				print("append ",G[edge[0]] )
 				path.append(edge[0])
-				if min_flow > edge[1] - edge[2]:
-					min_flow = edge[1] - edge[2]
+	return False
 
 
 
-def recovery_path(temp_path, min_flow, t):
-	print(min_flow)
-	print(temp_path)
+def recovery_path(temp_path,t):
+	#print(temp_path)
 	s = 0
-	path = []
 	while s != t:
 		prev = 0
 		for road in temp_path:
-			for edge in G[prev]:
-				if road == edge[0]:
-					if min_flow <= edge[1] - edge[2]:
-						s = road
-						path.append(s)
-						prev = road
-						#print("if")
-					else:
-						print("else")
-						temp_path.remove(prev)
-	print(path)
+			if G[prev] == []:
+				temp_path.remove(prev)
+			else:
+				possible_road = False
+				for edge in G[prev]:
+					if road == edge[0]:
+						#print("e1 {}, e2 {}".format(edge[1], edge[2]))
+						if edge[1] > edge[2]:
+							s = road
+							prev = road
+							possible_road = True
+				if possible_road is False:
+					if prev == 0:
+						return False
+
+					print(temp_path, " ", prev)
+					temp_path.remove(prev)
+
+	#print(temp_path)
+	return temp_path
+
+
+def find_min_flow(path):
+	min_flow = 10000
+	prev = 0
+	for road in path:
+		for edge in G[prev]:
+			if edge[0] == road:
+				prev = edge[0]
+				if min_flow > edge[1] - edge[2]:
+					min_flow = edge[1] - edge[2]
+	return min_flow
+
+
+def start_flow(min_flow, path):
+	global G
+	prev = 0
+	for road in path:
+		for edge in G[prev]:
+			if edge[0] == road:
+				prev = edge[0]
+				edge[2] += min_flow
+
 
 
 
 
 
 def main():
-	n = 6
-	t = 5
+	n = 5
+	t = 4
 	global U, G
 	#add flow to graph
 	for node in G:
 		for edge in node:
 			edge.append(0)
 
-	U = [False] * n
-	temp_path, min_flow = dfs(t)
-	recovery_path(temp_path, min_flow, t)
+	while True:
+		U = [False] * n
+		temp_path = dfs(t)
+		if temp_path is False:
+			return
+		path = recovery_path(temp_path, t)
+		if path is False:
+			return
+		min_flow = find_min_flow(path)
+		start_flow(min_flow, path)
+		print("min {} path {}".format(min_flow, path))
 
 if __name__ == "__main__":
 	main()
